3e108e6c90adcbf7c9a8e07bea604e1c18353f7f
Nanda <nandakumarpurohit@gmail.com>
2016-02-10 16:01:07 -0500

    web files...

---
+++ src/com/marlabs/demo/Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.marlabs.demo;
+
+import java.util.Date;
+
+/**
+ * @author Nanda
+ *
+ */
+public class Test {
+	public String getData() {
+		return new Date().toString();
+	}
+}
